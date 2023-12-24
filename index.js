1a2e778b3ae924752b039be0e768302c29a92df3
Zohir Bala <zohirbala@Zohirs-Air.broadband>
2023-12-24 23:34:03 +0000

    logic resolved

--- index.js
+++ index.js
@@ -10,84 +10,97 @@ const customTip = document.getElementById("custom");
 const reset = document.getElementById("reset");
 
 let resetCalculator = false;
+let percentage;
 
 // Outputs
 
 const tipAmount = document.getElementById("tip");
 const totalTipAmount = document.getElementById("total");
 
-// Tip input trigger
-const handleTipInput = (e) => {
-    const clickedElement = e.target; // Get the clicked element (button or custom input)
-    const isCustomTip = clickedElement.id === "custom";
+// Event listener for tipOptions buttons
 
-    e.preventDefault(); // Prevent default behavior for both events
+        tipOptions.forEach(button => {
+            button.addEventListener("click", (e) => {
+                if (e.type !== "click") {
+                    return; // Ignore keyup events that were causing me issues
+                }
+        
+                e.preventDefault();
+        
+                if (!numberOfPeople.value || numberOfPeople.value === "0") {
+                    numberOfPeople.parentElement.classList.add("border-2", "border-red-300");
+                } else {
+                    const tipPercentage = e.target.dataset.percentage;
+                    console.log("Button clicked, percentage: " + tipPercentage);
+                    numberOfPeople.parentElement.classList.remove("border-2", "border-red-300");
+                    percentage = tipPercentage;
+                    calculateTip();
+                    resetCalculator = true;
+                }
+            });
+        });
 
-    if (!numberOfPeople.value || numberOfPeople.value === "0") {
-        numberOfPeople.parentElement.classList.add("border-2", "border-red-300");
-        ; // Exit if number of people is invalid
-    } else if(isCustomTip) {
-        let tipPercentage = parseFloat(customTip.value);
-        if (isNaN(tipPercentage) || tipPercentage === 0) {
+
+
+// Event listener for customTip input using enter key
+
+document.addEventListener("keyup", (e) => {
+    if (e.key === "Enter" && e.target === customTip) {
+        const customTipPercentage = parseFloat(customTip.value);
+
+        if (!numberOfPeople.value || numberOfPeople.value === "0") {
+            numberOfPeople.parentElement.classList.add("border-2", "border-red-300");
+        } else if (isNaN(customTipPercentage) || customTipPercentage === 0) {
             customTip.classList.add("border-2", "border-red-300");
-    } else {
-        tipPercentage = clickedElement.dataset.percentage;
-        numberOfPeople.parentElement.classList.remove("border-2", "border-red-300");
-        calculateTip(billAmount.value, numberOfPeople.value, tipPercentage);
-        resetCalculator = true;
+            numberOfPeople.parentElement.classList.remove("border-2", "border-red-300");
+        } else {
+            console.log("This is the custom percentage: " + customTipPercentage);
+            percentage = customTipPercentage;
+            calculateTip();
+            resetCalculator = true;
+        }
     }
-    }
-};
+});
 
-// Attach event listeners to both tip options and custom tip
-tipOptions.forEach(button => button.addEventListener("click", handleTipInput));
-customTip.addEventListener("keyup", handleTipInput);
-    
 
 // Reset button 
-    reset.addEventListener("click", ()=>{
-        billAmount.value = "";
-        tipOptions.value = "";
-        numberOfPeople.value = "";
-        customTip.value = "";
-        tipAmount.innerHTML = "0";
-        totalTipAmount.innerHTML = "0";
-        resetCalculator = false;
-    });
-
-
-
-
-
-
-
-
+reset.addEventListener("click", () => {
+  billAmount.value = "";
+  tipOptions.value = "";
+  numberOfPeople.value = "";
+  customTip.value = "";
+  tipAmount.innerHTML = "0";
+  totalTipAmount.innerHTML = "0";
+  resetCalculator = false;
+});
 
 // Tip calculator 
 
-function calculateTip(bill, people, percentage){
-        
-    if(!resetCalculator){
-        console.log("This is functions " + percentage) // Console ouput is 5
-        const finalBill = bill * (1 + (percentage/ 100))
-        
-        let totalTipPerPerson = finalBill / people;
-        let tipPerPerson = ((finalBill - bill)/ people)
-        
-        tipAmount.innerHTML = `£${tipPerPerson.toFixed(2)}`;
-        totalTipAmount.innerHTML = `£${totalTipPerPerson.toFixed(2)}`;
-        
-        // If the Bill is £ 0.00
-        if(isNaN(bill) || bill == "0"){
-            finalBill = 0;
-            totalTipAmount = 0;
-        };
+function calculateTip() {
+  const bill = billAmount.value;
+  const people = numberOfPeople.value;
 
-        
-        
-    }
-        
-    
+  if (!resetCalculator) {
+    console.log("This is functions " + percentage) // Console ouput is 5
+    const finalBill = bill * (1 + (percentage / 100))
+
+    let totalTipPerPerson = finalBill / people;
+    let tipPerPerson = ((finalBill - bill) / people)
+
+
+    // If the Bill is £ 0.00
+    if (isNaN(bill) || bill == "0") {
+      totalTipPerPerson = 0;
+      tipPerPerson = 0;
+    };
+
+    tipAmount.innerHTML = `£${tipPerPerson.toFixed(2)}`;
+    totalTipAmount.innerHTML = `£${totalTipPerPerson.toFixed(2)}`;
+
+
+  }
+
+
 }
 
 
